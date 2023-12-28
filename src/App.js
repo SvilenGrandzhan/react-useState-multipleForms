325553e5b9e86bcebc472de17f8e1fc853c64311
Svilen Grandzhan <peructep@gmail.com>
2023-12-28 16:28:07 -0700

    working on submit button 20231228

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Display from "./Display";
 import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Display />
     </div>
   );
 }
