3ae9c6c215c7036aab89e457701ad73d279be973
Svilen Grandzhan <peructep@gmail.com>
2024-01-09 17:45:35 -0700

    Started from beginning 20240109

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Display from "./Display";
-import Form from "./Form";
 
 function App() {
   return (
-    <div className="App">
-      <Form />
-      <Display />
-    </div>
+    <>
+      <p>paragraph</p>
+    </>
   );
 }
 
